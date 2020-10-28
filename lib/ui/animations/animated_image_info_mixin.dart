7236ce2598f99ea4dceac7cd832f4f29195a2b98
Olexii Strelnykov <olexiistrelnykov@appvesto.com>
2020-10-28 17:13:07 +0200

    change animated mixins

---
+++ lib/ui/animations/animated_image_info_mixin.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+import 'package:pictures_view/res/const.dart';
+
+mixin AnimatedImageInfoMixin<T extends StatefulWidget> on State<T> implements TickerProvider {
+  bool isOpened = false;
+
+  AnimationController _animationController;
+
+  Animation<double> _paddingAnimation;
+  Animation<double> _imageSizeAnimation;
+  Animation<double> _containerSizeAnimation;
+
+  double get getAnimatedPadding => _paddingAnimation?.value ?? 0;
+  double get getAnimatedImageSize=> _imageSizeAnimation?.value ?? 0;
+  double get getAnimatedContainerSize => _containerSizeAnimation?.value ?? 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this, // the SingleTickerProviderStateMixin
+      duration: kBaseChangeDuration,
+    )..addListener(() {
+        setState(() {});
+      });
+    WidgetsBinding.instance.addPostFrameCallback(_bindingsInitAnimation);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @nonVirtual
+  void _bindingsInitAnimation(Duration timeStamp) {
+    final double maxHeight = MediaQuery.of(context).size.height;
+    final double minPadding = maxHeight * 0.3;
+    final double maxPadding = maxHeight * 0.75;
+
+    final CurvedAnimation curvedAnimation = CurvedAnimation(parent: _animationController, curve: Curves.easeIn);
+
+    _paddingAnimation = Tween<double>(begin: maxPadding, end: minPadding).animate(curvedAnimation);
+    _containerSizeAnimation = Tween<double>(begin: minPadding, end: maxPadding).animate(curvedAnimation);
+    _imageSizeAnimation = Tween<double>(begin: maxPadding + 50.0, end: minPadding + 50.0).animate(curvedAnimation);
+    setState(() {});
+  }
+
+  void changePosition() {
+    isOpened = !isOpened;
+    if (isOpened) {
+      _animationController.forward();
+    } else {
+      _animationController.reverse();
+    }
+  }
+}
--- lib/ui/pages/image_info_page/widgets/image_info_body.dart
+++ lib/ui/pages/image_info_page/widgets/image_info_body.dart
@@ -6,6 +6,7 @@ import 'package:pictures_view/pikcha_main_lib.dart';
 import 'package:pictures_view/res/const.dart';
 
 import 'package:pictures_view/models/dtos/card_dto.dart';
+import 'package:pictures_view/ui/animations/animated_image_info_mixin.dart';
 import 'package:pictures_view/widgets/global_button.dart';
 
 import 'package:pictures_view/ui/pages/image_info_page/widgets/image_content.dart';
@@ -23,35 +24,26 @@ class ImageInfoBody extends ThemeStatefulWidget {
   State<StatefulWidget> createState() => ImageInfoBodyState();
 }
 
-class ImageInfoBodyState extends ThemeState<ImageInfoBody> with SingleTickerProviderStateMixin {
-  bool isOpened = false;
-
-  AnimationController _animationController;
+class ImageInfoBodyState extends ThemeState<ImageInfoBody> with SingleTickerProviderStateMixin, AnimatedImageInfoMixin {
   PhotoViewScaleStateController _scaleStateController;
-
-  Animation<double> _paddingAnimation;
-  Animation<double> _imageSizeAnimation;
-  Animation<double> _containerSizeAnimation;
 
   @override
   void initState() {
     super.initState();
     _scaleStateController = PhotoViewScaleStateController();
-    _animationController = AnimationController(
-      vsync: this, // the SingleTickerProviderStateMixin
-      duration: kBaseChangeDuration,
-    )..addListener(() {
-        setState(() {});
-      });
-
-    WidgetsBinding.instance.addPostFrameCallback(bindingsInitAnimation);
   }
 
   @override
   void dispose() {
-    _animationController.dispose();
     _scaleStateController.dispose();
     super.dispose();
+  }
+
+  @override
+  void changePosition() {
+    super.changePosition();
+    _scaleStateController.reset();
+    setState(() {});
   }
 
   @override
@@ -59,7 +51,7 @@ class ImageInfoBodyState extends ThemeState<ImageInfoBody> with SingleTickerProv
     return Stack(
       children: <Widget>[
         SizedBox(
-          height: _imageSizeAnimation?.value ?? 0,
+          height: getAnimatedImageSize,
           child: PhotoView(
             imageProvider: NetworkImage(widget.image.imageUrl),
             basePosition: Alignment.topCenter,
@@ -70,7 +62,7 @@ class ImageInfoBodyState extends ThemeState<ImageInfoBody> with SingleTickerProv
           ),
         ),
         Padding(
-          padding: EdgeInsets.only(top: _paddingAnimation?.value ?? 0),
+          padding: EdgeInsets.only(top: getAnimatedPadding),
           child: ClipRRect(
             borderRadius: BorderRadius.circular(25.0),
             child: Stack(
@@ -78,7 +70,7 @@ class ImageInfoBodyState extends ThemeState<ImageInfoBody> with SingleTickerProv
                 Padding(
                   padding: EdgeInsets.only(top: 25.0),
                   child: Container(
-                    height: _containerSizeAnimation?.value ?? 0,
+                    height: getAnimatedContainerSize,
                     width: double.infinity,
                     decoration: BoxDecoration(
                       color: theme.colors.primaryColor,
@@ -115,31 +107,5 @@ class ImageInfoBodyState extends ThemeState<ImageInfoBody> with SingleTickerProv
         TransParentAppBar(isFavorite: widget.image.isLiked),
       ],
     );
-  }
-
-  void bindingsInitAnimation(Duration timeStamp) {
-    final double maxHeight = MediaQuery.of(context).size.height;
-    final double minPadding = maxHeight * 0.3;
-    final double maxPadding = maxHeight * 0.75;
-
-    final CurvedAnimation curvedAnimation = CurvedAnimation(parent: _animationController, curve: Curves.easeIn);
-
-    _paddingAnimation = Tween<double>(begin: maxPadding, end: minPadding).animate(curvedAnimation);
-    _containerSizeAnimation = Tween<double>(begin: minPadding, end: maxPadding).animate(curvedAnimation);
-    _imageSizeAnimation = Tween<double>(begin: maxPadding + 50.0, end: minPadding + 50.0).animate(curvedAnimation);
-    setState(() {});
-  }
-
-  void changePosition() {
-    isOpened = !isOpened;
-
-    if (isOpened) {
-      _animationController.forward();
-    } else {
-      _animationController.reverse();
-    }
-
-    _scaleStateController.reset();
-    setState(() {});
   }
 }
