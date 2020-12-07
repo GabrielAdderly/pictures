a2feddb0039415442075aa2aad9182255addf4aa
Denis Rebikov <denisrebikov@appvesto.com>
2020-12-07 17:14:26 +0200

    big crutchy mixin

--- lib/ui/pages/home_page/mixins/init_animations_mixin.dart
+++ lib/ui/pages/home_page/mixins/init_animations_mixin.dart
@@ -1,8 +1,6 @@
-
-
 import 'package:flutter/material.dart';
 
-mixin InitAnimationMixin {
+mixin InitAnimationMixin<T extends StatefulWidget> on State<T> {
   Animation animateTopPadding;
   Animation animateEvenRightPadding;
   Animation animateUnevenLeftPadding;
@@ -11,20 +9,23 @@ mixin InitAnimationMixin {
   AnimationController rightEvenPaddingController;
   AnimationController leftUnevenPaddingController;
   AnimationController rightUnevenPaddingController;
+  final ScrollController scrollController = ScrollController();
 
-  bool _isAnimatedForward = true;
   bool wasChecked = false;
+  bool showDebugPrints = false;
+  bool isAnimatedForward = true;
+
   double halfOfPrimaryWidth;
+  double cardHeightWithPadding;
 
-  void updatePage() {}
+  int gridScrollMultiplier = 0;
+  int listScrollMultiplier = 0;
+  double savedGridOffset = 0.0;
+  double savedListOffset = 0.0;
+  double viewsScrollDifference = 0.0;
 
+  Duration duration;
 
-  void initTween() {
-
-
-  }
-
-  void _compensateViewScrollChange() {}
 
   void initTopPaddingAnimation() {
     AnimationStatus animationStatus;
@@ -35,10 +36,10 @@ mixin InitAnimationMixin {
           print('FORWARD');
           _compensateViewScrollChange();
         }
-        if (animationStatus == AnimationStatus.reverse && !_isAnimatedForward) {
+        if (animationStatus == AnimationStatus.reverse && !isAnimatedForward) {
           _compensateViewScrollChange(reversed: true);
         }
-        updatePage();
+        setState(() {});
       })
       ..addStatusListener((AnimationStatus status) {
         animationStatus = status;
@@ -48,9 +49,108 @@ mixin InitAnimationMixin {
         } else if (status == AnimationStatus.dismissed) {
           print('DONE');
           wasChecked = false;
-          _isAnimatedForward = true;
-          _initBufferVariables();
+          isAnimatedForward = true;
+          initBufferVariables();
         }
       });
+  }
+
+  void initUnevenLeftPaddingAnimation() {
+    animateUnevenLeftPadding = Tween<double>(begin: halfOfPrimaryWidth, end: 0).animate(leftUnevenPaddingController);
+  }
+
+
+  void initUnevenRightPaddingAnimation() {
+    animateUnevenRightPadding = Tween<double>(begin: 0, end: halfOfPrimaryWidth).animate(rightUnevenPaddingController)
+      ..addListener(() => setState(() {}))
+      ..addStatusListener((AnimationStatus status) {
+        if (status == AnimationStatus.completed && isAnimatedForward) {
+          rightEvenPaddingController.forward();
+          rightUnevenPaddingController.reverse();
+        } else if (status == AnimationStatus.dismissed && !isAnimatedForward) {
+          print('REVERSE');
+          topPaddingController.reverse();
+        }
+      });
+  }
+
+  void initEvenRightPaddingAnimation() {
+    animateEvenRightPadding = Tween<double>(begin: halfOfPrimaryWidth, end: 0).animate(rightEvenPaddingController)
+      ..addListener(() => setState(() {}))
+      ..addStatusListener((AnimationStatus status) {
+        if (status == AnimationStatus.completed) {
+          print('DONE');
+          initBufferVariables();
+        }
+        if (status == AnimationStatus.dismissed && !isAnimatedForward) {
+          leftUnevenPaddingController.reverse();
+          rightUnevenPaddingController.reverse();
+        }
+      });
+  }
+
+  void initBufferVariables() {
+    savedGridOffset = 0.0;
+    savedListOffset = 0.0;
+    gridScrollMultiplier = 0;
+    listScrollMultiplier = 0;
+    viewsScrollDifference = 0.0;
+  }
+
+
+  void _compensateViewScrollChange({bool reversed = false}) {
+    if (savedGridOffset == 0.0) savedGridOffset = scrollController.offset;
+    if (viewsScrollDifference == 0.0) viewsScrollDifference = savedGridOffset % cardHeightWithPadding;
+    if (gridScrollMultiplier == 0) gridScrollMultiplier = scrollController.offset ~/ cardHeightWithPadding;
+
+    if (reversed) {
+      if (savedListOffset == 0.0) {
+        savedListOffset = (scrollController.offset - viewsScrollDifference) / 2 + viewsScrollDifference;
+      }
+      if (listScrollMultiplier == 0) listScrollMultiplier = scrollController.offset ~/ (2 * cardHeightWithPadding);
+
+      printDebug();
+      _jumpTo(savedListOffset, listScrollMultiplier);
+    } else {
+      printDebug();
+      _jumpTo(savedGridOffset, gridScrollMultiplier);
+    }
+  }
+
+  void unevenScroll(Function triggerTween) {
+    if (savedGridOffset == 0.0) savedGridOffset = scrollController.offset;
+    if (savedGridOffset ~/ cardHeightWithPadding % 2 != 0 && !wasChecked) {
+      wasChecked = true;
+
+      final Duration scrollToRowDuration = Duration(milliseconds: (duration.inMilliseconds / 4).round());
+
+      scrollController.animateTo(
+        savedGridOffset - cardHeightWithPadding,
+        duration: scrollToRowDuration,
+        curve: Curves.decelerate,
+      );
+
+      Future.delayed(scrollToRowDuration, () => triggerTween());
+    } else {
+      triggerTween();
+    }
+  }
+
+  void _jumpTo(double offset, int offsetMultiplier) {
+    scrollController.jumpTo(offset + animateTopPadding.value * offsetMultiplier);
+  }
+
+  void printDebug() {
+    if (showDebugPrints) {
+      print('Current offset ${scrollController.offset}');
+      print('Tween value ${animateTopPadding.value}');
+      print('Views scroll difference $viewsScrollDifference');
+      print('Saved list offset $savedListOffset');
+      print('Saved grid offset $savedGridOffset');
+      print('Current compensating jump offset ${savedListOffset + animateTopPadding.value * listScrollMultiplier}');
+      print('Card height + padding $cardHeightWithPadding');
+      print('List\'s compensating scroll multiplier $listScrollMultiplier');
+      print('Grid\'s compensating scroll multiplier $gridScrollMultiplier');
+    }
   }
 }
--- lib/ui/pages/home_page/widgets/animated_grid/animated_grid.dart
+++ lib/ui/pages/home_page/widgets/animated_grid/animated_grid.dart
@@ -32,54 +32,36 @@ class AnimatedGrid extends ThemeStatefulWidget {
 }
 
 class _AnimatedGridState extends ThemeState<AnimatedGrid> with TickerProviderStateMixin, InitAnimationMixin {
-  //bool _isAnimatedForward = true;
-  //bool wasChecked = false;
-  //double halfOfPrimaryWidth;
-  double cardHeightWithPadding;
-  int gridScrollMultiplier = 0;
-  int listScrollMultiplier = 0;
-  double savedGridOffset = 0.0;
-  double savedListOffset = 0.0;
-  double viewsScrollDifference = 0.0;
-//  Animation animateTopPadding;
-//  Animation animateEvenRightPadding;
-//  Animation animateUnevenLeftPadding;
-//  Animation animateUnevenRightPadding;
-//  AnimationController topPaddingController;
-//  AnimationController rightEvenPaddingController;
-//  AnimationController leftUnevenPaddingController;
-//  AnimationController rightUnevenPaddingController;
-  final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
+
+    duration = widget.duration;
+    wasChecked = widget.showDebugPrints;
+    final Duration stepDuration = Duration(milliseconds: (widget.duration.inMilliseconds / 4).round());
+
     final double pixelRatio = WidgetsBinding.instance.window.devicePixelRatio;
     halfOfPrimaryWidth = WidgetsBinding.instance.window.physicalSize.width / pixelRatio * 0.5;
-
     cardHeightWithPadding = halfOfPrimaryWidth * widget.childrenAspectRatio + widget.allSideChildrenPadding * 2;
 
     widget.toggleAnimationCallback(_toggleAnimation);
 
-    final Duration stepDuration = Duration(milliseconds: (widget.duration.inMilliseconds / 4).round());
+    topPaddingController = AnimationController(duration: stepDuration, vsync: this);
+    rightEvenPaddingController = AnimationController(duration: stepDuration, vsync: this);
+    leftUnevenPaddingController = AnimationController(duration: stepDuration, vsync: this);
+    rightUnevenPaddingController = AnimationController(duration: stepDuration, vsync: this);
 
-//    topPaddingController = AnimationController(duration: stepDuration, vsync: this);
-//    rightEvenPaddingController = AnimationController(duration: stepDuration, vsync: this);
-//    leftUnevenPaddingController = AnimationController(duration: stepDuration, vsync: this);
-//    rightUnevenPaddingController = AnimationController(duration: stepDuration, vsync: this);
-//
     initTopPaddingAnimation();
-    _initUnevenLeftPaddingAnimation;
-    _initUnevenRightPaddingAnimation;
-    _initEvenRightPaddingAnimation;
+    initUnevenLeftPaddingAnimation();
+    initUnevenRightPaddingAnimation();
+    initEvenRightPaddingAnimation();
   }
-
-
 
   @override
   Widget buildWidget(BuildContext context, CustomTheme theme) {
     return CleanedScrollView(
-      controller: _scrollController,
+      controller: scrollController,
       children: [
         Stack(
           children: [
@@ -146,144 +128,13 @@ class _AnimatedGridState extends ThemeState<AnimatedGrid> with TickerProviderSta
   void _toggleAnimation() {
     if (topPaddingController.value == 0.0) {
       topPaddingController.forward();
-      print('${_scrollController.offset}');
+      print('${scrollController.offset}');
     } else {
-      _isAnimatedForward = false;
-      _unevenScroll(() {
+      isAnimatedForward = false;
+      unevenScroll(() {
         rightEvenPaddingController.reverse();
         rightUnevenPaddingController.forward();
       });
     }
-  }
-
-//  void get _initTopPaddingAnimation {
-//    AnimationStatus animationStatus;
-//    animateTopPadding = Tween<double>(begin: 0, end: cardHeightWithPadding).animate(topPaddingController)
-//      ..addListener(() {
-//        if (animationStatus == null) _compensateViewScrollChange();
-//        if (animationStatus == AnimationStatus.forward) {
-//          print('FORWARD');
-//          _compensateViewScrollChange();
-//        }
-//        if (animationStatus == AnimationStatus.reverse && !_isAnimatedForward) {
-//          _compensateViewScrollChange(reversed: true);
-//        }
-//        setState(() {});
-//      })
-//      ..addStatusListener((AnimationStatus status) {
-//        animationStatus = status;
-//        if (status == AnimationStatus.completed) {
-//          leftUnevenPaddingController.forward();
-//          rightUnevenPaddingController.forward();
-//        } else if (status == AnimationStatus.dismissed) {
-//          print('DONE');
-//          wasChecked = false;
-//          _isAnimatedForward = true;
-//          _initBufferVariables();
-//        }
-//      });
-//  }
-
-  void get _initUnevenLeftPaddingAnimation {
-    animateUnevenLeftPadding = Tween<double>(begin: halfOfPrimaryWidth, end: 0).animate(leftUnevenPaddingController);
-  }
-
-  void get _initUnevenRightPaddingAnimation {
-    animateUnevenRightPadding = Tween<double>(begin: 0, end: halfOfPrimaryWidth).animate(rightUnevenPaddingController)
-      ..addListener(() => setState(() {}))
-      ..addStatusListener((AnimationStatus status) {
-        if (status == AnimationStatus.completed && _isAnimatedForward) {
-          rightEvenPaddingController.forward();
-          rightUnevenPaddingController.reverse();
-        } else if (status == AnimationStatus.dismissed && !_isAnimatedForward) {
-          print('REVERSE');
-          topPaddingController.reverse();
-        }
-      });
-  }
-
-  void get _initEvenRightPaddingAnimation {
-    animateEvenRightPadding = Tween<double>(begin: halfOfPrimaryWidth, end: 0).animate(rightEvenPaddingController)
-      ..addListener(() => setState(() {}))
-      ..addStatusListener((AnimationStatus status) {
-        if (status == AnimationStatus.completed) {
-          print('DONE');
-          _initBufferVariables();
-        }
-        if (status == AnimationStatus.dismissed && !_isAnimatedForward) {
-          leftUnevenPaddingController.reverse();
-          rightUnevenPaddingController.reverse();
-        }
-      });
-  }
-
-  void _compensateViewScrollChange({bool reversed = false}) {
-    if (savedGridOffset == 0.0) savedGridOffset = _scrollController.offset;
-    if (viewsScrollDifference == 0.0) viewsScrollDifference = savedGridOffset % cardHeightWithPadding;
-    if (gridScrollMultiplier == 0) gridScrollMultiplier = _scrollController.offset ~/ cardHeightWithPadding;
-
-    if (reversed) {
-      if (savedListOffset == 0.0) {
-        savedListOffset = (_scrollController.offset - viewsScrollDifference) / 2 + viewsScrollDifference;
-      }
-      if (listScrollMultiplier == 0) listScrollMultiplier = _scrollController.offset ~/ (2 * cardHeightWithPadding);
-
-      _printDebug();
-      _jumpTo(savedListOffset, listScrollMultiplier);
-    } else {
-      _printDebug();
-      _jumpTo(savedGridOffset, gridScrollMultiplier);
-    }
-  }
-
-  void _unevenScroll(Function triggerTween) {
-    if (savedGridOffset == 0.0) savedGridOffset = _scrollController.offset;
-    if (savedGridOffset ~/ cardHeightWithPadding % 2 != 0 && !wasChecked) {
-      wasChecked = true;
-
-      final Duration scrollToRowDuration = Duration(milliseconds: (widget.duration.inMilliseconds / 4).round());
-
-      _scrollController.animateTo(
-        savedGridOffset - cardHeightWithPadding,
-        duration: scrollToRowDuration,
-        curve: Curves.decelerate,
-      );
-
-      Future.delayed(scrollToRowDuration, () => triggerTween());
-    } else {
-      triggerTween();
-    }
-  }
-
-  void _jumpTo(double offset, int offsetMultiplier) {
-    _scrollController.jumpTo(offset + animateTopPadding.value * offsetMultiplier);
-  }
-
-  void _initBufferVariables() {
-    savedGridOffset = 0.0;
-    savedListOffset = 0.0;
-    gridScrollMultiplier = 0;
-    listScrollMultiplier = 0;
-    viewsScrollDifference = 0.0;
-  }
-
-  void _printDebug() {
-    if (widget.showDebugPrints) {
-      print('Current offset ${_scrollController.offset}');
-      print('Tween value ${animateTopPadding.value}');
-      print('Views scroll difference $viewsScrollDifference');
-      print('Saved list offset $savedListOffset');
-      print('Saved grid offset $savedGridOffset');
-      print('Current compensating jump offset ${savedListOffset + animateTopPadding.value * listScrollMultiplier}');
-      print('Card height + padding $cardHeightWithPadding');
-      print('List\'s compensating scroll multiplier $listScrollMultiplier');
-      print('Grid\'s compensating scroll multiplier $gridScrollMultiplier');
-    }
-  }
-
-  @override
-  void updatePage() {
-    setState(() {});
-    super.updatePage();
   }
 }
